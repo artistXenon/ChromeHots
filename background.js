ecb23d42448a395a93c87070b017a0e09edfd32c
Ailre <xtramainpp@gmail.com>
2023-07-12 16:50:57 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
 const i = setInterval(() => {
-    if (window == top) {
-        window.addEventListener('keydown', ({ altKey, code }) => {
+    if (window === top) {
+        window.addEventListener('keydown', ({ altKey, code, shiftKey }) => {
             const i = ((cd) => {
-                const c = /^Digit(\d{1})$/.exec(cd)
-                return (c && c[1] && !isNaN(c[1])) ? (parseInt(c[1]) - 1) : -1
-            })(code)
-            if (altKey && i > -1) chrome.runtime.sendMessage(undefined, i)
-        }, false)
-        clearInterval(i)
+                const c = /^Digit(\d{1})$/.exec(cd);
+                return (c && c[1] && !isNaN(c[1])) ? (parseInt(c[1]) - 1) : NaN;
+            })(code);
+            if (altKey && !isNaN(i)) chrome.runtime.sendMessage(undefined, 
+                JSON.stringify(
+                    {
+                        key: i,
+                        shift: shiftKey
+                    }
+                ))
+        }, false);
+        clearInterval(i);
     }    
-}, 1000)
+}, 1000);
